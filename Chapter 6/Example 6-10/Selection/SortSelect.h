cca3d8dac56c3e1f1efdc7aa5d496be747ea537f
CBreshears <clay.breshears@gmail.com>
2020-07-21 13:15:05 -0700

    Rename Chapter 6 directory

--- Chapter 6/Example 6-10/Selection/SortSelect.h
+++
@@ -1,6 +0,0 @@
-int SortSelect2(int [], int);
-int SortSelect3(int [], int);
-int SortSelect4(int [], int);
-int SortSelect5(int [], int);
-void CountAndMark(int [], int [], int , int , int []);
-void ArrayPack(int [], int [], int, int [], int);
