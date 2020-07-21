53a1e7527a9346204dae955dcad607c2a2035aa7
CBreshears <u42196@login-2.aidevcloud>
2020-07-21 13:02:25 -0700

    Add license statement to source files

--- Chapter_05/pi_pth.cc
+++ Chapter_05/pi_pth.cc
@@ -1,3 +1,36 @@
+/**
+ * @file pi_pth.cc
+ *
+ * @section LICENSE
+ *
+ * The MIT License
+ *
+ * @copyright Copyright (c) 2020 Clay Breshears (clay.breshears@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @section DESCRIPTION Pthreads implementation of numerical integration code 
+ *
+ * @section CITATION
+ *          Example 5-3 from "The Art of Concurrency" (O'Reilly Media, 2009)
+ **/
+
 #include <stdio.h>
 #include <pthread.h>
 
@@ -9,35 +42,34 @@ pthread_mutex_t gLock;
 
 void *threadFunction(void *pArg)
 {
-  int myNum = *((int *)pArg);
-  double partialHeight = 0.0, lWidth = 1.0 / NUM, x;
+   int myNum = *((int *)pArg);
+   double partialHeight = 0.0, lWidth = 1.0 / NUM, x;
 
-  for (int i = myNum; i < NUM; i += NUM_THREADS)
-  {
-    x = (i + 0.5f) / NUM;
-    partialHeight += 4.0f / (1.0f + x*x);
-  }
+   for (int i = myNum; i < NUM; i += NUM_THREADS) {
+      x = (i + 0.5f) / NUM;
+      partialHeight += 4.0f / (1.0f + x*x);
+   }
 
-  pthread_mutex_lock(&gLock);
+   pthread_mutex_lock(&gLock);
    gArea += partialHeight * lWidth;  
-  pthread_mutex_unlock(&gLock);
+   pthread_mutex_unlock(&gLock);
 }
 
 int main()
 {
-  pthread_t tHandles[NUM_THREADS];
-  int tNum[NUM_THREADS];
+   pthread_t tHandles[NUM_THREADS];
+   int tNum[NUM_THREADS];
 
-  pthread_mutex_init(&gLock, NULL);
-  for (int i = 0; i < NUM_THREADS; ++i) {
-    tNum[i] = i;
-    pthread_create(&tHandles[i], NULL, threadFunction, (void *)&tNum[i]); 
-  }
+   pthread_mutex_init(&gLock, NULL);
+   for (int i = 0; i < NUM_THREADS; ++i) {
+      tNum[i] = i;
+      pthread_create(&tHandles[i], NULL, threadFunction, (void *)&tNum[i]); 
+   }
 
-  for ( int j=0; j<NUM_THREADS; ++j ) {
-    pthread_join(tHandles[j], NULL);
-  }
+   for (int j = 0; j < NUM_THREADS; ++j) {
+      pthread_join(tHandles[j], NULL);
+   }
 
-  pthread_mutex_destroy(&gLock);
-  printf("Computed value of Pi: %f\n", gArea);
+   pthread_mutex_destroy(&gLock);
+   printf("The value of PI is %15.12f\n",gArea);
 }
--- Chapter_05/pi_tbb.cc
+++ Chapter_05/pi_tbb.cc
@@ -1,59 +1,79 @@
+/**
+ * @file pi_tbb.cc
+ *
+ * @section LICENSE
+ *
+ * The MIT License
+ *
+ * @copyright Copyright (c) 2020 Clay Breshears (clay.breshears@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * @section DESCRIPTION Threading Building Blocks implementation of numerical 
+ *                      integration code 
+ * @section CITATION
+ *          Example 5-2 from "The Art of Concurrency" (O'Reilly Media, 2009)
+ **/
+
 #include <stdio.h>
-#include <time.h>
-//#include <cassert>
-
-
-//#include <math.h>
-//#include <cstdlib>
-//#include <cctype>
 #include "tbb/parallel_reduce.h"
-#include "tbb/task_scheduler_init.h"
 #include "tbb/blocked_range.h"
 #include "tbb/partitioner.h"
 
 using namespace std;
 using namespace tbb;
 
-//#define Nthreads	2
-//#define GrainSize      10000
 long num_rects =  100000;
 
-
 class MyPi {
-	double *const my_rects;
+   double *const my_rects;
+
 public:
-	double partialHeight;
-	void operator()( const blocked_range<size_t>& r ) {
-		double rectangleWidth = *my_rects;
-		double x;
-	    for (size_t i = r.begin(); i != r.end(); ++i)
-		{
-			x = (i + .5)*rectangleWidth;
-			partialHeight += 4.0/(1.+ x*x);
-		}
-	}
+   double partialHeight;
 
-	MyPi( MyPi& x, split ) : my_rects(x.my_rects), partialHeight(0) {}
+   void operator()( const blocked_range<size_t>& r ) {
+      double rectangleWidth = *my_rects;
+      double x;
+      for (size_t i = r.begin(); i != r.end(); ++i) {
+         x = (i + .5)*rectangleWidth;
+         partialHeight += 4.0/(1.+ x*x);
+      }
+   }
 
-	void join( const MyPi& y ) {partialHeight += y.partialHeight;}
+   MyPi( MyPi& x, split ) : my_rects(x.my_rects), partialHeight(0) {}
+   
+   void join( const MyPi& y ) {partialHeight += y.partialHeight;}
 
-	MyPi(double *const width) : my_rects(width), partialHeight(0) {}
+   MyPi(double *const width) : my_rects(width), partialHeight(0) {}
 
 };
 
-
-
 int main(int argc, char* argv[])
 {
-	double area;
-	double width = 1./(double)num_rects;
-	MyPi my_block((double *const)&width);
-    task_scheduler_init init;
+   double area;
+   double width = 1./(double)num_rects;
+   MyPi my_block((double *const)&width);
 
-	parallel_reduce(blocked_range<size_t>(0,num_rects), my_block, auto_partitioner());
-	area = my_block.partialHeight*width;
+   parallel_reduce(blocked_range<size_t>(0,num_rects), my_block, auto_partitioner());
+   area = my_block.partialHeight*width;
 
-	printf("The value of PI is %15.12f\n",area);
+   printf("The value of PI is %15.12f\n",area);
 
-	return 0;
+   return 0;
 }
